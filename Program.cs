80b2ec5f64843c681525e2288d5d7fea5e22ba6c
Sasha <112004995+Sasha5151@users.noreply.github.com>
2022-09-28 16:30:57 +0300

    tt

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿//Натуральные числа в промежутке от 1 до N
+
+// int n = 8;
+// System.Console.WriteLine(Real(n));
+// string Real(int num)
+// {
+//     if (num == 1)
+//         return num.ToString();
+//     return Real(num - 1) + num.ToString();
+// }
+
+
+
+//Натуральные числа в промежутке от M до N
+
+// int m = 3;
+// int num = 8;
+// System.Console.WriteLine(Real(m , num));
+// string Real(int m , int num)
+// {
+//     if (num == m)
+//         return num.ToString();
+//     return Real(m ,num - 1) + num.ToString();
+// }
+
+////
+
+// int m = 8;
+// int num = 3;
+// System.Console.WriteLine(Real(m, num));
+// string Real(int m, int num)
+// {
+//     int a = -1;
+//     if (m > num) a = 1;
+//     if (num == m)
+//         return num.ToString();
+//     return Real(m, num + a) + num.ToString();
+// }
+
+
+//на вход число и возвращать сумму его цифр
+
+int num = 123;
+System.Console.WriteLine(Sum(num));
+Sum(num);
+int Sum(int num)
+{
+    if (num == 0)
+        return num;
+    return Sum(num / 10) + num % 10;
+}
+
